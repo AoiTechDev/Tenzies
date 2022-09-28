1c2e2213f9a36c31e47cde22394d0b282ef6f06d
Paweł Bornikowski <pawel.bornikowski@tlen.pl>
2022-09-28 19:42:09 +0200

    stworzenie glownego wrapera

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Dices from './components/Dices'
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Wraper = styled.div`
+  width: 40%;
+  height: 60%;
+  background: #ECECE7;
+  display: flex;
+  flex-direction: column;
+  border-radius: 2rem;
+  align-items: center;
+  h1{
+      font-size: 3rem;
+      font-weight: 500;
+  }
+`
 
+const Description = styled.div`
+  font-size: 1.3rem;
+  width: 80%;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+    <Wraper>
+        <h1>Tenzies</h1>
+        <Description>
+          Roll untill all dice are the same. Click each die to freeze it at its current value between rolls.
+        </Description>
+        
+    </Wraper>
+   </Container>
   );
 }
 
